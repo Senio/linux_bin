597e60f9d1f42318d0a35cff34bcc05684910834
Senio <arsenio699@gmail.com>
2017-06-09 11:18:01 +0800

    add docker-compose

--- install_docker_ce.sh
+++ install_docker_ce.sh
@@ -15,5 +15,8 @@ yum makecache fast
 
 yum install docker-ce -y
 
+curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 systemctl start docker
 systemctl enable docker
