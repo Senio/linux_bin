bd365c86bc7698e995b773055548e99fc5a9763a
Senio <arsenio699@gmail.com>
2017-10-21 22:07:29 +0800

    fix apt source list

--- debian_lxc_upgrade.sh
+++ debian_lxc_upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "deb http://httpredir.debian.org/debian stretch main" > /etc/apt/sources.list
-echo "deb http://httpredir.debian.org/debian stretch-updates main" > /etc/apt/sources.list
-echo "deb http://security.debian.org stretch/updates main" > /etc/apt/sources.list
+echo "deb http://httpredir.debian.org/debian stretch-updates main" >> /etc/apt/sources.list
+echo "deb http://security.debian.org stretch/updates main" >> /etc/apt/sources.list
 
 apt update && apt upgrade -y
