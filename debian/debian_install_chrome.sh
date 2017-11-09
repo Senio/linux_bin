8bfe2acf3bb6d42b204f8df6229508ce8299bcd7
Senio <arsenio699@gmail.com>
2017-11-10 02:03:59 +0800

    add selenium

--- debian/debian_install_chrome.sh
+++ debian/debian_install_chrome.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
+sudo apt install -y chromium-browser
+
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-sudo apt-get install -f
+sudo apt install -f
 
 rm -f google-chrome-stable_current_amd64.deb
 
---
+++ ubuntu/install_selenium.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt install -y chromium-browser
+
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+sudo apt install -f
+
+rm -f google-chrome-stable_current_amd64.deb
+
+wget https://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip
+
+unzip chromedriver_linux64.zip
+
+sudo mv chromedriver /usr/local/bin
+
+wget http://selenium-release.storage.googleapis.com/3.7/selenium-server-standalone-3.7.1.jar
+java -jar selenium-server-standalone-3.7.1.jar
