c5c7b4438da0b002d9009dc66c40f96016d72f6c
Senio <arsenio699@gmail.com>
2017-11-09 19:29:22 +0800

    add chrome

---
+++ debian/debian_install_chrome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+sudo apt-get install -f
+
+rm -f google-chrome-stable_current_amd64.deb
+
