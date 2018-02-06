ca8356e388ad03c1c08da9ccc46c66becdd9562d
Senio <you@example.com>
2018-02-06 14:24:40 +0800

    set timezone

--- debian/install_tmux.sh
+++ debian/install_tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install tmux -y
+sudo apt install tmux -y
 
 git clone https://github.com/gpakosz/.tmux ~/.tmux
 ln -s .tmux/.tmux.conf ~/
--- debian/lxc_upgrade.sh
+++ debian/lxc_upgrade.sh
@@ -4,6 +4,6 @@ echo "deb http://httpredir.debian.org/debian stretch main contrib" > /etc/apt/so
 echo "deb http://httpredir.debian.org/debian stretch-updates main contrib" >> /etc/apt/sources.list
 echo "deb http://security.debian.org stretch/updates main contrib" >> /etc/apt/sources.list
 
-apt update && apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
-apt install lsb-release -y
+sudo apt install lsb-release -y
