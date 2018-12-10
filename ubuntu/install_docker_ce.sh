90e5684654d4668e1e2ae7691842e1de2350788c
Senio <arsenio699@gmail.com>
2018-12-10 23:32:10 +0800

    updaet install_docker_ce

--- ubuntu/install_docker_ce.sh
+++ ubuntu/install_docker_ce.sh
@@ -13,7 +13,6 @@ sudo apt-get -y install \
 #curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-
 #sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
 	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -28,6 +27,8 @@ sudo apt-get update
 
 sudo apt-get install docker-ce -y
 
+sudo usermod -a -G docker senio
+
 ## Install docker-compose
 #https://docs.docker.com/engine/installation/linux/docker-ce/debian/#install-docker-compose-for-raspbian
 #curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
@@ -37,6 +38,7 @@ sudo apt-get install docker-ce -y
 sudo apt purge docker-compose -y
 rm -f /usr/local/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 docker-compose --version
+
