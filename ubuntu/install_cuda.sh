e772afa05b6bf0053c77ac6cf673282a2e812692
root <root@pve1.senio.tw>
2019-01-29 13:55:31 +0800

    add install cuda

---
+++ ubuntu/install_cuda.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo apt-get update
+sudo apt-get install cuda-9-0 cuda-9-2 -y
+
+
