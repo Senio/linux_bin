bb0f084715c304586d84f4e98475e92805104085
Senio <senio@Felix-MBP.local>
2020-03-10 15:41:16 +0800

    update openwrt tmux

--- openwrt/install_tmux.sh
+++ openwrt/install_tmux.sh
@@ -2,6 +2,6 @@
 
 opkg update && opkg install tmux
 
-git clone https://github.com/gpakosz/.tmux ~/.tmux
-ln -s .tmux/.tmux.conf ~/
-ln -s .tmux/.tmux.conf.local ~/
+# git clone https://github.com/gpakosz/.tmux ~/.tmux
+# ln -s .tmux/.tmux.conf ~/
+# ln -s .tmux/.tmux.conf.local ~/
