a0f73c74fca1a6f41b5a95e18fb8d2a835f1bba5
Senio <senio@Felix-MBP.local>
2020-04-08 11:09:23 +0800

    update ubuntu docker_ce

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#https://docs.docker.com/compose/install/
+# https://docs.docker.com/compose/install/
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
--- ubuntu/install_docker_ce.sh
+++ ubuntu/install_docker_ce.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#https://docs.docker.com/engine/installation/linux/docker-ce/debian/
+# https://docs.docker.com/engine/install/ubuntu/
+
+
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
 
 sudo apt-get update
 
+# 1. Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
@@ -10,10 +15,11 @@ sudo apt-get -y install \
     gnupg-agent \
     software-properties-common
 
-#curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
+# 2. Add Docker’s official GPG key:
+# curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-#sudo apt-key fingerprint 0EBFCD88
+# sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
 	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 	$(lsb_release -cs) \
@@ -35,14 +41,14 @@ sudo usermod -a -G docker senio
 # sudo apt-get install docker-compose -y
 
 # https://docs.docker.com/compose/install/#install-compose
-sudo apt purge docker-compose -y
-sudo rm -f /usr/local/bin/docker-compose
-# sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-docker-compose --version
+# sudo apt purge docker-compose -y
+# sudo rm -f /usr/local/bin/docker-compose
+# # sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+# # sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# # sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#
+# sudo chmod +x /usr/local/bin/docker-compose
+#
+# docker-compose --version
 
