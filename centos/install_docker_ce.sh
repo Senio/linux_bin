48a608b9faf79e987223c6bab5275aec477f111e
Senio <senio@Felix-MBP.local>
2020-06-03 15:28:17 +0800

    add centos8.1 install docker error

--- centos/install_docker_ce.sh
+++ centos/install_docker_ce.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 #https://docs.docker.com/compose/install/
 
-yum remove docker \
+# yum remove docker \
+#   docker-common \
+#   container-selinux \
+#   docker-selinux \
+#   docker-engine -y
+
+sudo yum remove docker \
+  docker-client \
+  docker-client-latest \
   docker-common \
-  container-selinux \
-  docker-selinux \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
   docker-engine -y
 
-yum install -y yum-utils
+sudo yum install -y yum-utils
 
-yum-config-manager \
+sudo yum-config-manager \
   --add-repo \
   https://download.docker.com/linux/centos/docker-ce.repo
 
 yum makecache fast
 
-yum install docker-ce -y
+# https://blog.csdn.net/shana_8/article/details/105190368
+# Error:
+# Problem: package docker-ce-3:19.03.8-3.el7.x86_64 requires containerd.io >= 1.2.2-3, but none of the providers can be installed
+# 此时需要安装新版containerd.io
+# dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+
+# yum install docker-ce -y
 
 # curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 # sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 systemctl start docker
 systemctl enable docker
