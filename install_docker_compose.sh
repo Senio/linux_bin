f6d453107f2b1bdd064a45c48f6a910e1cc541f8
Senio <senio@senio.tw>
2020-08-13 11:18:59 +0800

    update docker-compose

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # https://docs.docker.com/compose/install/
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
