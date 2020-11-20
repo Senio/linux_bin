e67f4355b542ccf991193156d42f7a8739f72ca9
Senio <senio@senio.tw>
2020-11-20 22:39:41 +0800

    update set_proxy.sh

--- debian/set_proxy.sh
+++ debian/set_proxy.sh
@@ -3,5 +3,5 @@
 git config --global http.proxy http://proxy:3128
 
 echo 'Acquire::http::Proxy "http://proxy:3128/";' | sudo tee /etc/apt/apt.conf.d/proxy.conf
-echo 'Acquire::http::Proxy "https://proxy:3128/";' | sudo tee -a /etc/apt/apt.conf.d/proxy.conf
+echo 'Acquire::https::Proxy "http://proxy:3128/";' | sudo tee -a /etc/apt/apt.conf.d/proxy.conf
 
