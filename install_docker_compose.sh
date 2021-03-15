d616450c5c2dfeb88128ad6c79b4130390dd0996
Senio <senio@senio.tw>
2021-03-15 11:25:39 +0800

    update docker-compse

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -2,7 +2,9 @@
 # https://docs.docker.com/compose/install/
 
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
 
 sudo chmod +x /usr/local/bin/docker-compose
 
