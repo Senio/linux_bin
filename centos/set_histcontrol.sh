e464aa72a5e947e5addc53a2eec110f278a46276
Senio <senio@senio.tw>
2021-03-19 11:50:38 +0800

    add zsh sethis

--- centos/set_histcontrol.sh
+++ centos/set_histcontrol.sh
@@ -5,3 +5,8 @@ if [ $? -ne 0 ] ; then
   echo 'export HISTCONTROL=ignoreboth' >> ~/.bashrc
 fi
 
+grep 'export HISTCONTROL=ignoreboth' ~/.zshrc
+if [ $? -ne 0 ] ; then
+  echo 'export HISTCONTROL=ignoreboth' >> ~/.zshrc
+fi
+
