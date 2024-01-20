0b703f614a7eb2d6f83d17f02ad441ffb11eeab0
Senio Wang <senio.wang@senio.tw>
2024-01-20 14:10:50 +0800

    update

--- debian/custom_install.sh
+++ debian/custom_install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-sh ./lxc_upgrade.sh
+# sh ./lxc_upgrade.sh
+
+sudo apt update
+sudo apt install -y git
+
 sh ./install_docker_ce.sh
 sh ./install_tmux.sh
 sh ./install_ohmyzsh.sh
 
-apt install -y pydf
+sudo apt install -y pydf
---
+++ debian/install_ms.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cat /etc/*-release
+# curl -sSL -O https://packages.microsoft.com/config/<distribution>/<version>/packages-microsoft-prod.deb
+curl -sSL -O https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb
+curl -sSL -O https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb
+
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install libfuse3-dev fuse3 -y
+
+sudo apt-get install blobfuse2
+
+sudo blobfuse2 twse ~/twse --config-file=./config.yaml
+# blobfuse2 mount /data1 --config-file=/home/az-senio/config.yaml --container-name=twse --log-level=log_debug --log-file-path=./bobfuse2b.log
+blobfuse2 mount /data1 --config-file=/home/az-senio/config.yaml
+
+# fstab
+/<path_to_blobfuse2_mount.sh_file>/mount.sh   </path/to/desired/mountpoint>     fuse    defaults,_netdev 0 0
